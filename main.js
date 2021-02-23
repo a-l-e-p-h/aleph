b2f88acfd75399592b63d69566d0f6b8d1d2629f
Alex Gohorel <gohorelalex@gmail.com>
2021-02-23 17:56:03 -0500

    simple snowpack + lit-element demo

---
+++ main.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow } = require("electron");
+const isDev = require("electron-is-dev");
+const path = require("path");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  const startURL = isDev
+    ? "http://localhost:8080"
+    : `file://${path.join(__dirname, "index.html")}`;
+
+  win.loadURL(startURL);
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
