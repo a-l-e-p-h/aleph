7a6ae703d01dee3cab94924c99442712dfb68f2c
Alex Gohorel <gohorelalex@gmail.com>
2021-04-26 21:38:13 -0400

    remove lit-element dep, fix remaining imports

--- ui/components/Window/Window.js
+++ ui/components/Window/Window.js
@@ -1,5 +1,5 @@
 import { LitElement, html } from "lit";
-import { styleMap } from "lit-html/directives/style-map";
+import { styleMap } from "lit/directives/style-map.js";
 
 import windowStyles from "./windowStyles";
 import "./WindowButton";
